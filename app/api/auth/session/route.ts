f6cfaed07dd928b339884e6eadbd8ea37c4ffbc3
Morymirco <morykoulibaly996@gmail.com>
2025-05-14 10:09:38 +0000

    cookie

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,27 +1,27 @@
-// import { cookies } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function POST(request: NextRequest) {
-//   const { token } = await request.json();
+export async function POST(request: NextRequest) {
+  const { token } = await request.json();
   
-//   // Définir le cookie de session avec le token Firebase
-//   const cookieStore = cookies();
-//   cookieStore.set({
-//     name: "session",
-//     value: token,
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     maxAge: 60 * 60 * 24 * 5, // 5 jours
-//     path: "/",
-//   });
+  // Définir le cookie de session avec le token Firebase
+  const cookieStore = await cookies();
+  cookieStore.set({
+    name: "session",
+    value: token,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 5, // 5 jours
+    path: "/",
+  });
   
-//   return NextResponse.json({ success: true });
-// }
+  return NextResponse.json({ success: true });
+}
 
-// export async function DELETE() {
-//   // Supprimer le cookie de session
-//   const cookieStore = cookies();
-//   cookieStore.delete("session");
+export async function DELETE() {
+  // Supprimer le cookie de session
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
   
-//   return NextResponse.json({ success: true });
-// } 
+  return NextResponse.json({ success: true });
+} 
